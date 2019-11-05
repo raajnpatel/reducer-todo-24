2fe18d177a5934c2b6217001678eaebbbe27837e
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-05 13:00:14 -0800

    App refactored

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,11 +1,14 @@
-export const initialState =
-    {
-        item: 'Learn about reducers',
-        completed: false,
-        id: 3892987589
-    };
-
-export const reducer = (state, action) => {
+export const initialState = {
+    todoArray:
+        [
+            {
+                item: 'Learn about reducers',
+                completed: false,
+                id: Date.now(),
+            }
+        ]
+};
+    export const reducer = (state, action) => {
     switch(action.type){
         default:
             return state;
